40c0d44bb8e574e8b177d3f38f70cca0af136136
Saad Ismail <me@saadismail.net>
2016-12-30 15:43:25 +0500

    Added support for GR and subsections in GR

--- custom/print.php
+++ custom/print.php
@@ -6,18 +6,26 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
 define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
-
 if (isset($_GET["section"]) && isset($_GET["batch"])) {
     $section = $_GET["section"];
-    if (strlen($section) > 1) {
-        die('Section must be of 1 alphabet');
+    if (strlen($section) > 3 || !preg_match("/^[a-i]$/i", $section) && !preg_match("/^GR[1-9]$/", $section) ) {
+        die('Invalid section');
     }
+
     $batch = $_GET["batch"];
     if ($batch > 16 || $batch < 13) {
-        die('I am only taught for Batch 13 to Batch 16');
+        die('Invalid batch (13-16)');
     }
 } else {
     die('Stop playing with me!!');
+}
+
+date_default_timezone_set('Asia/Karachi');
+$julianday = gregoriantojd(date('m'),date('d'),date('Y'));
+$day_of_week = jddayofweek($julianday);
+
+if ($day_of_week > 5) {
+    die('Enjoy the weekend');
 }
 
 # Assign colors for batch
@@ -32,15 +40,6 @@ if ($batch == 16) {
 } else {
     die('Something went wrong');
 }
-
-date_default_timezone_set('Asia/Karachi');
-$julianday = gregoriantojd(date('m'),date('d'),date('Y'));
-$day_of_week = jddayofweek($julianday);
-
-if ($day_of_week > 5) {
-    die('Enjoy the weekend');
-}
-
 /** PHPExcel_IOFactory */
 require_once dirname(__FILE__) . '/../Classes/PHPExcel/IOFactory.php';
 
@@ -48,22 +47,19 @@ $objReader = PHPExcel_IOFactory::createReader('Excel2007');
 $objPHPExcel = $objReader->load("./BSCS.xlsx");
 $worksheet = $objPHPExcel->setActiveSheetIndex(($day_of_week-1));
 echo "<table style='text-align: center' border='4' class='stats' cellspacing='0'>";
-//foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
-    echo "<tr><th>Room</th><th>Timing</th><th>Subject</th></tr>";
-	foreach ($worksheet->getColumnIterator() as $column) {
-		$cellIterator = $column->getCellIterator();
-		foreach ($cellIterator as $cell) {
-			if (!is_null($cell) && !is_null($cell->getCalculatedValue()) && $cell->getStyle()->getFill()->getStartColor()->getRGB() == $color) {
-                if (strpos($cell->getCalculatedValue(), '-'.$section) !== false) {Note:
-//                if (preg_match('/-I/', $cell->getCalculatedValue()) ) {
-                    $colindex = substr($cell->getCoordinate(), 0, 1);
-                    $rowindex = substr($cell->getCoordinate(), 1, 2);
-                    $timing =  $objPHPExcel->getActiveSheet()->getCell($colindex.'3')->getValue();
-                    $room = $objPHPExcel->getActiveSheet()->getCell('A'.$rowindex)->getValue();
-                    $subject = $cell->getCalculatedValue();
-                    echo "<tr><td>".$room.'</td><td>'.$timing.'</td><td>'.$subject."</tr>".EOL;
-                }
-			}
-		}
-	}
-//}
+echo "<tr><th>Room</th><th>Timing</th><th>Subject</th></tr>";
+foreach ($worksheet->getColumnIterator() as $column) {
+    $cellIterator = $column->getCellIterator();
+    foreach ($cellIterator as $cell) {
+        if (!is_null($cell) && !is_null($cell->getCalculatedValue()) && $cell->getStyle()->getFill()->getStartColor()->getRGB() == $color) {
+            if (strripos($cell->getCalculatedValue(), '-'.$section) !== false || strripos($cell->getCalculatedValue(), ','.$section) !== false || strripos($cell->getCalculatedValue(), $section.',') !== false) {
+                $colindex = substr($cell->getCoordinate(), 0, 1);
+                $rowindex = substr($cell->getCoordinate(), 1, 2);
+                $timing =  $objPHPExcel->getActiveSheet()->getCell($colindex.'3')->getValue();
+                $room = $objPHPExcel->getActiveSheet()->getCell('A'.$rowindex)->getValue();
+                $subject = $cell->getCalculatedValue();
+                echo "<tr><td>".$room.'</td><td>'.$timing.'</td><td>'.$subject."</tr>".EOL;
+            }
+        }
+    }
+}
