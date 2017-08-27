e4b2d63bd30dec054f9cedf34a82202256abf2d9
Saad Ismail <me@saadismail.net>
2017-08-27 20:48:16 +0500

    Update timetable to v1.3

--- test.php
+++ test.php
@@ -1,14 +1,22 @@
 <?php
 
-function cmp($a, $b)
-{
-	$timing1 = $a; $timing2 = $b;
-    if ($timing1 >= 8 && $timing2 >= 8 || $timing1 <= 3 && $timing2 <= 3) {
-        return ($timing1 < $timing2) ? -1 : 1;
-    }
-    return ($timing1 < $timing2) ? 1 : -1;
+// If you are not using Composer
+require("sendgrid-php/sendgrid-php.php");
 
-}
+$from = new SendGrid\Email("Timetable Notifier", "notify@timetable.host");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Saad Ismail", "saad3112@gmail.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-echo cmp(3, 12);
+$apiKey = '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';
+$sg = new \SendGrid($apiKey);
+
+echo $apiKey;
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+print_r($response->headers());
+echo $response->body();
+
 ?>
